f9a3f328e6e697ba6ec0e1f79777b514347a7f5d
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-18 07:04:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 
 document.querySelector("main").remove();
 let newHeader = document.createElement("h1");
+<<<<<<< HEAD
 newHeader.id="victory";
 document.body.appendChild(newHeader);
 
 // let newHeader = document.querySelector("h1#victor");
+=======
+newHeader.id="victor";
+document.body.appendChild(newHeader);
+
+let newHeader = document.querySelector("h1#victor");
+>>>>>>> 3e041608cd40c54db08aaa98239673592855ff5d
 
 newHeader.innerHTML = "YOUR-NAME is the champion";
 
